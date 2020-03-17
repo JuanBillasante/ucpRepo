5e7aa43f62f5d74b39767663a15d4142b1e56098
JuanBillasante <juantaguidici@hotmail.com>
2020-03-16 22:37:07 -0300

    SumaYResta

--- src/calc.ts
+++ src/calc.ts
@@ -1,9 +1,24 @@
-class Calculadora {
-
-    constructor() {
+export class Calculador {
+    constructor(){
+    }
+    sumar(a : number, b : number):number{
+        let c=0;
+        
+        for(a=a;a>0;a--){
+            for(c=b;c==b;b++){
+            }
+        }
+        return b;
     }
 
-    static sumar(a: number, b: number): number {
-        return a + b;
+    resta(a : number, b : number):number{
+        let c=0;
+        
+        for(b=b;b>0;b--){
+            for(c=a;c==a;a--){
+            }
+        }
+        return a;
     }
+
 }
--- test/calc.test.ts
+++ test/calc.test.ts
@@ -1,8 +1,18 @@
 import { expect } from "chai";
+import { Calculador } from "../src/calc";
 
-describe ('Calculador',function(){
-    it ('Debe sumar 5',function(){
-     let result = Calculadora.sumar(2,3);
-     expect(result).equal(5);
+describe('calculate', function() {
+    it('Sumador', function() {
+      const c = new Calculador();
+      let result = c.sumar(2, 5);
+      expect(result).equal(7);
     });
-});
+  });
+
+  describe('calculate', function() {
+    it('Restador', function() {
+      const c = new Calculador();
+      let result = c.resta(7, 3);
+      expect(result).equal(4);
+    });
+  });
