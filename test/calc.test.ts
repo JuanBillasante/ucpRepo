e9814b84dd40e1cb5f9a9261dec8e40be6a9d82b
Juan Billasante <juan.billasante@registrocivilctes.gov.ar>
2020-04-07 19:17:42 -0300

    testWorkflow 2

--- test/calc.test.ts
+++ test/calc.test.ts
@@ -1,26 +1,10 @@
 import { expect } from "chai";
-import { Calculador } from "../src/Calc";
 import 'mocha';
 import { AccionReceptor } from "../src/AccionReceptor";
 import { Parseador } from "../src/Parseador";
 
 
 
-describe('calculate', function() {
-    it('Sumar', function() {
-      const c = new Calculador();
-      let result = c.sumar(2, 5);
-      expect(result).equal(7);
-    });
-  });
-
-  describe('calculate', function() {
-    it('Restar', function() {
-      const c = new Calculador();
-      let result = c.restar(7, 3);
-      expect(result).equal(4);
-    });
-  });
 
     describe('accionNum1', function(){
       it('Accion', function(){
